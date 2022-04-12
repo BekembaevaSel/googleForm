61a468d526e950a66536a168393745f468aaee83
BekembaevaSel <bekembaeva8@gmail.com>
2022-04-12 19:44:10 +0600

    calculator using reducer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import TodoForm from './components/TodoForm'
-import WelcomePage from './components/WelcomePage'
-import { useSelector } from 'react-redux'
-import SinglePage from './components/SinglePage'
-function App() {
-	const todoList = useSelector((state) => state.todos.todoList)
+import React from 'react'
+import CalcForm from './components/CalcForm'
 
-	return (
-		<Routes>
-			<Route path='/' element={<WelcomePage />} />
-			<Route path='/todo' element={<TodoForm  />} />
-			<Route
-				path='todo/:id'
-				element={<SinglePage/>}
-			/>
-		</Routes>
-	)
+const App = () => {
+  return (
+	<>
+	<CalcForm />
+	</>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { Provider } from 'react-redux'
-import store from './store/store'
-import { BrowserRouter } from 'react-router-dom'
+// import { Provider } from 'react-redux'
+// import store from './store/store'
+// import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-	<BrowserRouter>
-		<Provider store={store}>
+const root = createRoot(document.getElementById('root'))
+root.render(
+	// <BrowserRouter>
+	// 	<Provider store={store}>
 			<React.StrictMode>
 				<App />
-			</React.StrictMode>
-		</Provider>
-	</BrowserRouter>,
-	document.getElementById('root'),
+			</React.StrictMode>,
+		{/* </Provider>
+	</BrowserRouter> */}
+	
 )
+
+// ReactDOM.render(
+// 	<BrowserRouter>
+// 		<Provider store={store}>
+// 			<React.StrictMode>
+// 				<App />
+// 			</React.StrictMode>
+// 		</Provider>
+// 	</BrowserRouter>
+// 	,
+// 	document.getElementById('root'),
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
